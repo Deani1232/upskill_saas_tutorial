b3fd7814871ae6fd00cd3f27f493e98b0e4d051e
Dean Irwin <Deani1232@gmx.com>
2017-06-08 18:12:03 +0000

    Added comments into contact controller, linked contact controller to contact mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,8 +4,26 @@ class ContactsController < ApplicationController
     end
     
     def create
+        #Create a new contact with the params passed in the private code block below
+        #It is assigned this way for security purposes.
+        #The contact @instanceVariable is referring to the new contact object created
+        #above. The contact created above is following a blueprint laid out in the model folder.
+        
         @contact = Contact.new(contact_params)
         if @contact.save
+            
+            #Assign these variables to send email.
+            name = params[:contact][:name]
+            email = params[:contact][:name]
+            body = params[:contact][:comments]
+            
+            #Mail with assigned variables above and using the method defined in the mailer folderr
+            #for contact mailer. The ContactMailer class is defined in the mailers folder.
+            ContactMailer.contact_email(name, email, body).deliver
+            
+            #Let the user know that it was successful by sending the message to new_contact_path
+            #The view will interpret how the flash appears. This view is in the application view and
+            #is accessable on each page.
             flash[:success] = "Message Sent."
             redirect_to new_contact_path
         else
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,4 @@
+#Define a contact mailer class to use elsewhere in the program.
 class ContactMailer < ActionMailer::Base
     default to: 'Deani1232@gmx.com'
     
@@ -8,4 +9,5 @@ class ContactMailer < ActionMailer::Base
         
         mail(from: email, subject: 'Contact Form Message')
     end
+    
 end
