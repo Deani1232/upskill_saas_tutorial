0e48f83e2ecee583e9005297bd289cb4cd19041b
Dean Irwin <Deani1232@gmx.com>
2017-06-09 05:24:47 +0000

    Added plan variables and commands to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
+    #GET Request for /
     def home
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
+    
     def about
     end
 end
