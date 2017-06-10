4bdb60b08ae9c0c1d851b20e09c91173cc40740d
Dean Irwin <Deani1232@gmx.com>
2017-06-10 23:39:06 +0000

    Added code comments, and fixed up some bugs with payment processing.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,19 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+    
+    # Extend default devise gem behavior so that users signing up with the
+    # pro account (plan ID 2) save with a special Stripe Subscription.
+    # Otherwise, Devise signs up user as usual.
+    
+    def create
+        super do |resource|
+            if params[:plan]
+                resource.plan_id = params[:plan]
+                if resource.plan_id == 2
+                    resource.save_with_subscription
+                else
+                    resource.save
+                end
+            end
+        end
+    end
+end
