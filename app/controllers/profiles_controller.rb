de3aa7e4b6e5cb003ad0f703d163662f10158314
Dean Irwin <Deani1232@gmx.com>
2017-06-13 22:00:54 +0000

    Added profile edit actiona and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -21,6 +21,15 @@ class ProfilesController < ApplicationController
         end
     end
     
+    #GET Requests made to /users/:user_id/profile/edit
+    def edit
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+    end
+    
+    def update
+    end
+    
     private
         def profile_params
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description, :avatar)
